0ffc62e2c9ae0ba864bc6b96e8fa89bf1445ef66
WillyReyno <reyno.willy@gmail.com>
2017-01-28 22:40:07 +0100

    Initial commit

---
+++ includes/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+
+    <title>IIM A2 2020 - WEB - Laravel </title>
+
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/app.css" rel="stylesheet">
+    <link href="assets/css/prism.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <script src="assets/js/prism.js"></script>
+</head>
+
+<body>
+
+<div class="blog-masthead">
+    <div class="container">
+        <nav class="blog-nav">
+            <a class="blog-nav-item active" href="../index.php">Jour 1</a>
+            <a class="blog-nav-item" href="day-2">Jour 2</a>
+            <a class="blog-nav-item" href="day-3">Jour 3</a>
+            <a class="blog-nav-item" href="day-4">Jour 4</a>
+        </nav>
+    </div>
+</div>
+
+<div class="container">
+
